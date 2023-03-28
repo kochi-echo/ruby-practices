eeb20ff1ebd5d28583f8d4d54b884f211e54ff4f
kochi-echo <tnodtegrof@gmail.com>
2023-03-29 08:38:49 +0900

    一応クラスで書いてみた

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,38 +1,62 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-input = ARGV[0]
+class CalScore
+	attr_writer :max_pin
+	attr_reader :sum_score, :max_frame
 
-full_pin = 10
-score = 0 # 合計スコア
-add_array = [] # ストライクやスペアで追加で可算する回数が要素として入る
-pins_frame_array = [] # 1フレームで倒したピンの数が要素として入る(ストライク以外なら2要素)
-frame = 0 # フレーム数
-max_frame = 10
+	def initialize
+		@score = 0 # 合計スコア
+		@add_array = [] # ストライクやスペアで追加で可算する回数が要素として入る
+		@pins_frame_array = [] # 1フレームで倒したピンの数が要素として入る(ストライク以外なら2要素)
+		@max_frame = 10
+		@sum_score = 0
+		@frame_num = 0
+	end
 
-input_array = input.gsub('X', full_pin.to_s).split(',').map(&:to_i) # X->10に置換
+	def update_score(pin)
+		@pins_frame_array.push(pin)
+		if @pins_frame_array[0] == @max_pin && @frame_num < @max_frame # 前フレームがmax_frame以下の時にストライク判定
+			@frame_num += 1 # ストライクの時は1フレーム追加
+		elsif @frame_num < @max_frame
+			@frame_num += 0.5 # ストライク以外の時は半フレーム追加
+		end
 
-input_array.each do |pin|
-  pins_frame_array.push(pin)
-  if pins_frame_array[0] == full_pin && frame < max_frame # 前フレームがmax_frame以下の時にストライク判定
-    frame += 1 # ストライクの時は1フレーム追加
-  elsif frame < max_frame
-    frame += 0.5 # ストライク以外の時は半フレーム追加
-  end
+		add_score(pin)
+		update_add_array()
 
-  add_array.reject! { |elem| elem <= 0 }
-  score += (add_array.size + 1) * pin
-  add_array.map! { |elem| elem - 1 if elem.positive? }
+		@pins_frame_array = [] if @pins_frame_array.size > 1 || pin == @max_pin # 2回投げるかストライクを取ると1フレーム中の倒したピンはリセット
 
-  if pins_frame_array[0] == full_pin && frame < max_frame # 現フレームがmax_frame以下の時にストライク判定
-    add_array.push(2) # 2回分点数追加
-  elsif pins_frame_array.sum == full_pin && frame < max_frame # 現フレームがmax_frame以下の時にスペア判定
-    add_array.push(1) # 1回分点数追加
-  end
+  	puts "#{@frame_num}回目: #{pin}倒して合計は#{@sum_score} （#{@add_array.size}個分追加で足された）" if @frame_num%1 == 0
+	end
 
-  pins_frame_array = [] if pins_frame_array.size > 1 || pin == full_pin # 2回投げるかストライクを取ると1フレーム中の倒したピンはリセット
+	def add_score(pin)
+		@add_array.reject! { |elem| elem <= 0 }
+		@sum_score += (@add_array.size + 1) * pin
+		@add_array.map! { |elem| elem - 1 if elem.positive? }
+	end
 
-  # puts "#{frame}回目: #{pin}倒して合計は#{score} （#{add_array.size}個分追加で足された）" if frame%1 == 0
+	def update_add_array
+		if @pins_frame_array[0] == @max_pin && @frame_num < @max_frame # 現フレームがmax_frame以下の時にストライク判定
+			@add_array.push(2) # 2回分点数追加
+		elsif @pins_frame_array.sum == @max_pin && @frame_num < @max_frame # 現フレームがmax_frame以下の時にスペア判定
+			@add_array.push(1) # 1回分点数追加
+		end
+	end
+
 end
 
-puts score
+full_pin = 10
+
+input = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5'
+# input = ARGV[0]
+input_array = input.gsub('X', full_pin.to_s).split(',').map(&:to_i) # X->10に置換
+
+score_data = CalScore.new
+score_data.max_pin = full_pin
+
+input_array.each do |pin|
+	score_data.update_score(pin)
+end
+
+puts score_data.sum_score
---
+++ 03.bowling/test.rb
@@ -0,0 +1,11 @@
+class Test
+  def initialize
+    @a = 1
+    puts @a
+  end
+end
+
+obj = Test.new
+
+b = []
+puts b.sum
