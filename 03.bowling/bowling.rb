dd131cccb5453e1b15d8bf29f987dd1e984c71b8
kochi-echo <tnodtegrof@gmail.com>
2023-03-28 21:13:21 +0900

    平文で要件は満たした

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -27,30 +27,21 @@
 
 # 方針
 - 小さく作る
-  - 入力は'X,4,4', '4,6,4', '4,3,4'
-  - まずはeachを使わず、未来も見通せることとする
-
-- 10回ループ
-- 以下のクラスをメソッドを置く
-  - ストライク時の処理
-  - スペア時の処理
-  - 普通の時の処理
-
+    - 入力は'X,4,4', '4,6,4', '4,3,4'
+    - まずはeachを使わず、未来も見通せることとする -> if文だけで書けるが、'X,X,4,4'に対応できなかった
+- 基本構成
+    - 基本は倒したピンの可算
+    - strike、spareになるごとにメソッドを呼び出す ->いらなかった
+    - strike時: strike後2回分倒したピンを可算する
+        - strikeをしたらcount=2を生成
+        - 次の周でcount>0だったら現在の倒したピンを可算し、countを1減らす
+        課題：どうやってストライクが発生するたびに新たなcountを生成し、ループを超えて生き残らせるか？
+        ->sum_count.push!(2)をして、sum_count.sizeかけるピン数追加で可算していけば良い
+    - spare時: sum_count.push!(1)をして、sum_count.sizeかけるピン数追加で可算していけば良い
 =end
 
 require 'debug'
 
-
-
-def calc_score(pin, sum_score, sum_count)
-    if sum_count > 0
-        sum_score += pin * 2
-        sum_count -= 1
-    else
-        sum_score += pin
-    end
-    {count: sum_count, score: sum_score}
-end
 
 input_data = [
 '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5',
@@ -61,12 +52,13 @@ input_data = [
 'X,X,X,X,X,X,X,X,X,X,X,X'
 ]
 
-input = input_data[2]
+input = input_data[4]
+
 puts input
 
 full_pin = 10
 score = 0
-sum_count = 0
+sum_count = []
 previous_pin = 0
 throw_count = []
 times = 0
@@ -75,32 +67,30 @@ max_frame = 10
 input_array = input.gsub('X', full_pin.to_s).split(',').map{ |elem| elem.to_i }
 
 input_array.each_with_index{ |pin, index|
-    # if pin == full_pin
-        # debuggerß
-    # end
-    throw_count.push(pin)
 
-    if (throw_count[0] == full_pin && times < max_frame)
-        times += 1
-    elsif times < 10
-        times += 0.5
-    end
-    
-    if (throw_count[0] == full_pin && times < max_frame)
-        score = calc_score(pin, score, sum_count)[:score]
-        sum_count += 2
+        throw_count.push(pin)
+        if (throw_count[0] == full_pin && times < max_frame)
+            times += 1
+        elsif times < 10
+            times += 0.5
+        end
 
-    elsif (throw_count.sum == full_pin && times < max_frame)
-        score = calc_score(pin, score, sum_count)[:score]
-        sum_count = 1  
+        sum_count.reject!{|elem| elem <= 0}
+        score += sum_count.size * pin
+        score += pin
+        sum_count.map!{|elem| elem - 1}
 
-    else
-        result = calc_score(pin, score, sum_count)
-        score = result[:score]
-        sum_count = result[:count]
-    end
-    throw_count = [] if (throw_count.size > 1 || pin == 10)
-    puts "#{times}回目: #{pin}倒して合計#{score} 次の#{sum_count}分足される" if times%1 == 0
+        if (throw_count[0] == full_pin && times < max_frame)
+            sum_count.push(2)
+
+        elsif (throw_count.sum == full_pin && times < max_frame)
+            sum_count.push(1)
+        end
+
+        throw_count = [] if (throw_count.size > 1 || pin == full_pin)
+
+        # puts "#{times}回目: #{pin}倒して合計#{score} 次の#{sum_count.size}分足される" if times%1 == 0
+        # p sum_count
 }
 
 puts score
