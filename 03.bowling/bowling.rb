0ec213ce806428bc4a5af8aa3dcdeb579dd5e87d
kochi-echo <tnodtegrof@gmail.com>
2023-04-12 06:54:45 +0900

    pairsが二次元配列出ない時を考慮

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -49,7 +49,7 @@ end
 
 def separate_frame(all_pins)
   all_pins.each_with_object([]) do |pin, pairs|
-    if pairs.empty? || pairs.size < MAX_FRAME && (pairs.last.size >= BASIC_SIZE_1FRAME || pairs.last.last >= MAX_PIN)
+    if pairs.empty? || pairs.size < MAX_FRAME && (pairs.last&.size >= BASIC_SIZE_1FRAME || pairs.last&.last >= MAX_PIN)
       pairs << [pin]
     else
       pairs.last << pin
