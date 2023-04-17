ed5826978b3b390f04adde9db7af797bbacbc9cf
kochi-echo <tnodtegrof@gmail.com>
2023-04-17 15:44:01 +0900

    テストにクラスの変更を反映

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -12,7 +12,7 @@ class Frame
     @points = []
     @previous_frame = previous_frame
     @pre_previous_frame = pre_previous_frame
-    @frame_num = 1
+    @frame_num = (previous_frame&.frame_num || 0) + 1
   end
 
   def score
@@ -27,10 +27,10 @@ class Frame
     @points.size < BASIC_SIZE_1FRAME && @points[0] == MAX_PIN
   end
 
-  def max_throw_each_frame?(throw_num, all_pins)
-    self.throw_max_before_last_frame? || final_frame?(throw_num, all_pins)
+  def max_throw_normal_frame?
+    (@points.size >= BASIC_SIZE_1FRAME || self.strike?) && @frame_num < MAX_FRAME
   end
-  
+
   private
 
   def score_spare
@@ -44,45 +44,39 @@ class Frame
   def score_strike
     if @previous_frame&.strike?
       sum = @points[0..1].sum
-      sum += @points[0] if @pre_previous_frame&.strike? && !a.nil?
+      sum += (@points[0] || 0) if @pre_previous_frame&.strike?
       sum
     else
       0
     end
   end
-
-  def throw_max_before_last_frame?
-    @points.size >= BASIC_SIZE_1FRAME || self.strike? && @frame_num < MAX_FRAME
-  end
-
-  def final_frame?(throw_num, all_pins_size)
-    throw_num + 1 >= all_pins_size
-  end
 end
 
 def calculate_score(all_pins)
   total_score = 0
-  frame = Frame.new
   previous_frame = nil
+  pre_previous_frame = nil
+  frame = Frame.new
 
   all_pins.each_with_index do |point, throw_num|
     frame.points << point
-    if frame.max_throw_each_frame?(throw_num, all_pins.size)
-      total_score = frame.score
+    final_frame = throw_num + 1 >= all_pins.size
+
+    if frame.max_throw_normal_frame? || final_frame
+      total_score += frame.score
       now_frame = frame
       frame = Frame.new(now_frame, previous_frame)
       previous_frame = now_frame
-      frame.frame_num = now_frame.frame_num + 1
     end
   end
   total_score
 end
 
-def generate_score(input)
+def convert_input_to_score(input)
   all_pins = input.gsub('X', MAX_PIN.to_s).split(',').map(&:to_i) # X->10に置換
   calculate_score(all_pins)
 end
 
 input = ARGV[0]
-puts generate_score(input) unless input.nil?
+puts convert_input_to_score(input) unless input.nil?
 
--- 03.bowling/test.rb
+++ 03.bowling/test.rb
@@ -42,90 +42,87 @@ class TestFrameMethod < Minitest::Test
     assert frame.strike?
   end
 
-  def test_throw_max_each_frame
+  def test_max_throw_normal_frame
     frame = Frame.new
     frame.points = [3]
-    refute frame.throw_max_each_frame?(1, 20)
+    refute frame.max_throw_normal_frame?
     frame = Frame.new
     frame.points = [2, 8]
-    assert frame.throw_max_each_frame?(2, 20)
+    assert frame.max_throw_normal_frame?
     frame = Frame.new
     frame.points = [10]
-    assert frame.throw_max_each_frame?(1, 20)
+    assert frame.max_throw_normal_frame?
 
     frame.points = [3]
     frame.frame_num = 10
-    refute frame.throw_max_each_frame?(1, 20)
+    refute frame.max_throw_normal_frame?
     frame = Frame.new
     frame.points = [2, 8]
     frame.frame_num = 10
-    refute frame.throw_max_each_frame?(2, 20)
+    refute frame.max_throw_normal_frame?
     frame = Frame.new
     frame.points = [10]
     frame.frame_num = 10
-    refute frame.throw_max_each_frame?(1, 20)
-
-    frame.points = [2, 8, 3]
-    assert frame.throw_max_each_frame?(19, 20)
+    refute frame.max_throw_normal_frame?
   end
 end
 
 
 
-# class TestSmallScoreCalculation < Minitest::Test
-#   def test_calc_normal_score
-#     all_pins = [1, 2, 3, 4]
-#     assert_equal 10, calculate_score(all_pins)
-#     all_pins = [1, 2, 3]
-#     assert_equal 6, calculate_score(all_pins)
-#   end
+class TestSmallScoreCalculation < Minitest::Test
+  def test_calc_normal_score
+    all_pins = [1, 2, 3, 4]
+    assert_equal 10, calculate_score(all_pins)
+    all_pins = [1, 2, 3]
+    assert_equal 6, calculate_score(all_pins)
+  end
 
-#   def test_calc_spare_score
-#     all_pins = [1, 9, 3, 4]
-#     assert_equal 20, calculate_score(all_pins)
-#     all_pins = [1, 9, 3]
-#     assert_equal 16, calculate_score(all_pins)
-#     all_pins = [0, 10, 1]
-#     assert_equal 12, calculate_score(all_pins)
-#   end
+  def test_calc_spare_score
+    all_pins = [1, 9, 3, 4]
+    assert_equal 20, calculate_score(all_pins)
+    all_pins = [1, 9, 3]
+    assert_equal 16, calculate_score(all_pins)
+    all_pins = [0, 10, 1]
+    assert_equal 12, calculate_score(all_pins)
+  end
 
-#   def test_calc_strike_score
-#     all_pins = [10, 3, 4]
-#     assert_equal 24, calculate_score(all_pins)
-#     all_pins = [10, 3]
-#     assert_equal 16, calculate_score(all_pins)
-#     all_pins = [10, 2, 3, 10, 2, 3]
-#     assert_equal 40, calculate_score(all_pins)
-#     all_pins = [10, 10, 10, 2, 3]
-#     assert_equal 35 + 20 + 12 + 5, calculate_score(all_pins)
-#     all_pins = [10, 10, 10, 2]
-#     assert_equal 32 + 20 + 12 + 2, calculate_score(all_pins)
-#   end
-# end
+  def test_calc_strike_score
+    all_pins = [10, 3, 4]
+    assert_equal 24, calculate_score(all_pins)
+    all_pins = [10, 3]
+    assert_equal 16, calculate_score(all_pins)
+    all_pins = [10, 2, 3, 10, 2, 3]
+    assert_equal 40, calculate_score(all_pins)
+    all_pins = [10, 10, 10, 2, 3]
+    assert_equal 35 + 20 + 12 + 5, calculate_score(all_pins)
+    all_pins = [10, 10, 10, 2]
+    assert_equal 32 + 20 + 12 + 2, calculate_score(all_pins)
+  end
+end
 
-# class TestGenerationScore < Minitest::Test
-#   def test_strike_and_spare
-#     assert_equal [6, 3, 9, 0, 0, 3, 8, 2].sum, generate_score('6,3,9,0,0,3,8,2')
-#     assert_equal [[6, 3], [9, 0], [0, 3], [8, 2], [7, 3], [10]].flatten.sum + 7 + 10, generate_score('6,3,9,0,0,3,8,2,7,3,X')
-#     assert_equal [[6, 3], [9, 0], [0, 3], [8, 2], [7, 3], [10], [9, 1], [8, 0]].flatten.sum + 7 + 10 + 10 + 8, generate_score('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0')
-#     assert_equal [[6, 3], [9, 0], [0, 3], [8, 2], [7, 3], [10], [9, 1], [8, 0], [10]].flatten.sum + 7 + 10 + 10 + 8, generate_score('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X')
-#     assert_equal 139, generate_score('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5')
-#   end
+class TestGenerationScore < Minitest::Test
+  def test_strike_and_spare
+    assert_equal [6, 3, 9, 0, 0, 3, 8, 2].sum, convert_input_to_score('6,3,9,0,0,3,8,2')
+    assert_equal [[6, 3], [9, 0], [0, 3], [8, 2], [7, 3], [10]].flatten.sum + 7 + 10, convert_input_to_score('6,3,9,0,0,3,8,2,7,3,X')
+    assert_equal [[6, 3], [9, 0], [0, 3], [8, 2], [7, 3], [10], [9, 1], [8, 0]].flatten.sum + 7 + 10 + 10 + 8, convert_input_to_score('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0')
+    assert_equal [[6, 3], [9, 0], [0, 3], [8, 2], [7, 3], [10], [9, 1], [8, 0], [10]].flatten.sum + 7 + 10 + 10 + 8, convert_input_to_score('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X')
+    assert_equal 139, convert_input_to_score('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5')
+  end
 
-#   def test_last_frame_3throw
-#     assert_equal 164, generate_score('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X')
-#   end
+  def test_last_frame_3throw
+    assert_equal 164, convert_input_to_score('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X')
+  end
 
-#   def test_3strike
-#     assert_equal [0, 10, 1, 5, 0, 0, 0, 0].sum + 1, generate_score('0,10,1,5,0,0,0,0')
-#     assert_equal [0, 10, 1, 5, 0, 0, 0, 0, 10, 10, 10, 5].sum + 1 + [10, 10].sum + [10, 5].sum + 5, generate_score('0,10,1,5,0,0,0,0,X,X,X,5')
-#     assert_equal [0, 10, 1, 5, 0, 0, 0, 0, 10, 10, 10, 5, 1].sum + 1 + [10, 10].sum + [10, 5].sum + [5, 1].sum, generate_score('0,10,1,5,0,0,0,0,X,X,X,5,1')
-#     assert_equal 107, generate_score('0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4')
-#   end
+  def test_3strike
+    assert_equal [0, 10, 1, 5, 0, 0, 0, 0].sum + 1, convert_input_to_score('0,10,1,5,0,0,0,0')
+    assert_equal [0, 10, 1, 5, 0, 0, 0, 0, 10, 10, 10, 5].sum + 1 + [10, 10].sum + [10, 5].sum + 5, convert_input_to_score('0,10,1,5,0,0,0,0,X,X,X,5')
+    assert_equal [0, 10, 1, 5, 0, 0, 0, 0, 10, 10, 10, 5, 1].sum + 1 + [10, 10].sum + [10, 5].sum + [5, 1].sum, convert_input_to_score('0,10,1,5,0,0,0,0,X,X,X,5,1')
+    assert_equal 107, convert_input_to_score('0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4')
+  end
 
-#   def test_other_scores
-#     assert_equal 134, generate_score('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0')
-#     assert_equal 144, generate_score('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,1,8')
-#     assert_equal 300, generate_score('X,X,X,X,X,X,X,X,X,X,X,X')
-#   end
-# end
+  def test_other_scores
+    assert_equal 134, convert_input_to_score('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0')
+    assert_equal 144, convert_input_to_score('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,1,8')
+    assert_equal 300, convert_input_to_score('X,X,X,X,X,X,X,X,X,X,X,X')
+  end
+end
