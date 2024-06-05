446a9d9d22319afbf502d0c8cae43bc8ab714de6
kochi-echo <tnodtegrof@gmail.com>
2024-06-06 08:31:07 +0900

    first_commit

---
+++ 05.wc/lib/bin_wc.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'optparse'
+
+require_relative 'bin_wc_command'
+
+options = {}
+opt = OptionParser.new
+opt.on('-l') { options[:a] = true }
+opt.on('-w') { options[:r] = true }
+opt.on('-c') { options[:l] = true }
+opt.parse!(ARGV) # オプション除いて残った引数
+input = ARGV[0]
+
+path_name = File.expand_path(input || '.')
+
+puts run_wc(path_name)
---
+++ 05.wc/test/test.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../lib/bin_wc'
+
+class SampleTest < Minitest::Test
+  def test_sample
+
+  end
+end
