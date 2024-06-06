af8fc3457b491cdd6f1f827c28d03c77e0988ada
kochi-echo <tnodtegrof@gmail.com>
2024-06-06 11:40:36 +0900

    複数オプションテストパス

--- 05.wc/test/test.rb
+++ 05.wc/test/test.rb
@@ -22,4 +22,25 @@ class WCTest < Minitest::Test
     ].join('\n')
     assert_equal expected, run_wc(DIRECTORY_PATHNAME + '*.txt', {})
   end
+
+  def test_wc_one_line_file_l_option
+    assert_equal '       1 one_line.txt', run_wc(DIRECTORY_PATHNAME + 'one_line.txt', {l: true})
+  end
+
+  def test_wc_one_line_file_w_option
+    assert_equal '       3 one_line.txt', run_wc(DIRECTORY_PATHNAME + 'one_line.txt', {w: true})
+  end
+
+  def test_wc_one_line_file_c_option
+    assert_equal '       6 one_line.txt', run_wc(DIRECTORY_PATHNAME + 'one_line.txt', {c: true})
+  end
+
+  def test_wc_one_line_file_multiple_options
+    assert_equal '       1       3       6 one_line.txt', run_wc(DIRECTORY_PATHNAME + 'one_line.txt', {l: true, w: true, c: true})
+    assert_equal '       1       3 one_line.txt', run_wc(DIRECTORY_PATHNAME + 'one_line.txt', {l: true, w: true})
+    assert_equal '       3       6 one_line.txt', run_wc(DIRECTORY_PATHNAME + 'one_line.txt', {w: true, c: true})
+    assert_equal '       1       6 one_line.txt', run_wc(DIRECTORY_PATHNAME + 'one_line.txt', {l: true, c: true})
+  end
+
+
 end
