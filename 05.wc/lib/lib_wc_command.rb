eb456db630748ff3d8cfa72b9123d221738c1b5a
kochi-echo <tnodtegrof@gmail.com>
2024-06-07 11:44:10 +0900

    命名変更

--- 05.wc/lib/lib_wc_command.rb
+++ 05.wc/lib/lib_wc_command.rb
@@ -2,24 +2,24 @@
 # frozen_string_literal: true
 
 def run_wc(inputs, options, input_type_argv)
-  files_data = files_data(inputs, input_type_argv)
-  display_selection = display_selection(options)
-  text = align_data(files_data, display_selection)
-  text.push(calculate_total(files_data, display_selection)) if files_data.size > 1
+  files_data = collect_files_data(inputs, input_type_argv)
+  display_keys = select_display_keys(options)
+  text = format_data(files_data, display_keys)
+  text.push(format_total(files_data, display_keys)) if files_data.size > 1
   text.join("\n")
 end
 
-def files_data(inputs, input_type_argv)
+def collect_files_data(inputs, input_type_argv)
   if input_type_argv
-    targets_path = inputs.map { |input| Dir.glob(input) }.flatten
-    build_data(targets_path)
+    paths = inputs.flat_map { |input| Dir.glob(input) }
+    build_data(paths)
   else
     [file_data(inputs, '')]
   end
 end
 
-def display_selection(options)
-  no_option = options.values.count(true).zero?
+def select_display_keys(options)
+  no_option = options.values.none?
   {
     row_number: no_option || options[:l],
     word_number: no_option || options[:w],
@@ -27,37 +27,37 @@ def display_selection(options)
   }.select { |_key, value| value }.keys
 end
 
-def build_data(targets_path)
-  targets_path.map do |target_path|
-    next { warning: "wc: #{target_path}: read: Is a directory" } if File.directory?(target_path)
+def build_data(paths)
+  paths.map do |path|
+    next { warning: "wc: #{path}: read: Is a directory" } if File.directory?(path)
 
-    io = File.open(target_path)
-    content = io.read
-    file_data(content, target_path)
-  end.compact
+    file = File.open(path)
+    content = file.read
+    file_data(content, path)
+  end
 end
 
-def file_data(content, target_path)
+def file_data(content, path)
   {
     row_number: content.lines.count,
     word_number: content.split.size,
     bytesize: content.bytesize,
-    file_name: target_path
+    file_name: path
   }
 end
 
-def align_data(files_data, display_selection)
+def format_data(files_data, display_keys)
   files_data.map do |file_data|
     next file_data[:warning] if file_data.key?(:warning)
 
-    text = display_selection.map { |key| file_data[key].to_s.rjust(8) }
+    text = display_keys.map { |key| file_data[key].to_s.rjust(8) }
     text.push(" #{file_data[:file_name]}") unless file_data[:file_name].empty?
     text.join
   end
 end
 
-def calculate_total(files_data, display_selection)
-  text = display_selection.map do |key|
+def format_total(files_data, display_keys)
+  text = display_keys.map do |key|
     files_data.sum { |file_data| file_data.key?(key) ? file_data[key] : 0 }.to_s.rjust(8)
   end
   text.push(' total').join
