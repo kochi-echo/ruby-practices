163fb20a6d4b7845082f480aab2cadd3c210f820
kochi-echo <tnodtegrof@gmail.com>
2024-06-13 18:22:05 +0900

    1ファイル入力時の配列無くす

--- 05.wc/test/test.rb
+++ 05.wc/test/test.rb
@@ -6,12 +6,12 @@ require_relative '../lib/lib_wc_command'
 
 class WCTest < Minitest::Test
   def test_wc_one_line_file
-    assert_equal '       1       3       6 ./05.wc/test/test_target/test_dir/one_line.txt', run_wc(['./05.wc/test/test_target/test_dir/one_line.txt'], nil, {})
+    assert_equal '       1       3       6 ./05.wc/test/test_target/test_dir/one_line.txt', run_wc('./05.wc/test/test_target/test_dir/one_line.txt', nil, {})
   end
 
   def test_wc_3_lines_file
     assert_equal '       3       3       9 ./05.wc/test/test_target/test_dir/three_lines.txt',
-                 run_wc(['./05.wc/test/test_target/test_dir/three_lines.txt'], nil, {})
+                 run_wc('./05.wc/test/test_target/test_dir/three_lines.txt', nil, {})
   end
 
   def test_wc_all_txt
@@ -20,7 +20,7 @@ class WCTest < Minitest::Test
       '       3       3       9 ./05.wc/test/test_target/test_dir/three_lines.txt',
       '       4       6      15 total'
     ].join("\n")
-    assert_equal expected, run_wc(['./05.wc/test/test_target/test_dir/*.txt'], nil, {})
+    assert_equal expected, run_wc('./05.wc/test/test_target/test_dir/*.txt', nil, {})
   end
 
   def test_wc_two_files
@@ -39,30 +39,30 @@ class WCTest < Minitest::Test
       '       2       3      19 ./05.wc/test/test_target/test_dir/two_lines.md',
       '       6       9      34 total'
     ].join("\n")
-    assert_equal expected, run_wc(['./05.wc/test/test_target/test_dir/*'], nil, {})
+    assert_equal expected, run_wc('./05.wc/test/test_target/test_dir/*', nil, {})
   end
 
   def test_wc_one_line_file_l_option
-    assert_equal '       1 ./05.wc/test/test_target/test_dir/one_line.txt', run_wc(['./05.wc/test/test_target/test_dir/one_line.txt'], nil, { l: true })
+    assert_equal '       1 ./05.wc/test/test_target/test_dir/one_line.txt', run_wc('./05.wc/test/test_target/test_dir/one_line.txt', nil, { l: true })
   end
 
   def test_wc_one_line_file_w_option
-    assert_equal '       3 ./05.wc/test/test_target/test_dir/one_line.txt', run_wc(['./05.wc/test/test_target/test_dir/one_line.txt'], nil, { w: true })
+    assert_equal '       3 ./05.wc/test/test_target/test_dir/one_line.txt', run_wc('./05.wc/test/test_target/test_dir/one_line.txt', nil, { w: true })
   end
 
   def test_wc_one_line_file_c_option
-    assert_equal '       6 ./05.wc/test/test_target/test_dir/one_line.txt', run_wc(['./05.wc/test/test_target/test_dir/one_line.txt'], nil, { c: true })
+    assert_equal '       6 ./05.wc/test/test_target/test_dir/one_line.txt', run_wc('./05.wc/test/test_target/test_dir/one_line.txt', nil, { c: true })
   end
 
   def test_wc_one_line_file_multiple_options
     assert_equal '       1       3       6 ./05.wc/test/test_target/test_dir/one_line.txt',
-                 run_wc(['./05.wc/test/test_target/test_dir/one_line.txt'], nil, { l: true, w: true, c: true })
+                 run_wc('./05.wc/test/test_target/test_dir/one_line.txt', nil, { l: true, w: true, c: true })
     assert_equal '       1       3 ./05.wc/test/test_target/test_dir/one_line.txt',
-                 run_wc(['./05.wc/test/test_target/test_dir/one_line.txt'], nil, { l: true, w: true })
+                 run_wc('./05.wc/test/test_target/test_dir/one_line.txt', nil, { l: true, w: true })
     assert_equal '       3       6 ./05.wc/test/test_target/test_dir/one_line.txt',
-                 run_wc(['./05.wc/test/test_target/test_dir/one_line.txt'], nil, { w: true, c: true })
+                 run_wc('./05.wc/test/test_target/test_dir/one_line.txt', nil, { w: true, c: true })
     assert_equal '       1       6 ./05.wc/test/test_target/test_dir/one_line.txt',
-                 run_wc(['./05.wc/test/test_target/test_dir/one_line.txt'], nil, { l: true, c: true })
+                 run_wc('./05.wc/test/test_target/test_dir/one_line.txt', nil, { l: true, c: true })
   end
 
   def test_wc_warning
@@ -71,7 +71,7 @@ class WCTest < Minitest::Test
       '       2       3      15 ./05.wc/test/test_target/three_word.md',
       '       2       3      15 total'
     ].join("\n")
-    assert_equal expected, run_wc(['./05.wc/test/test_target/*'], nil, {})
+    assert_equal expected, run_wc('./05.wc/test/test_target/*', nil, {})
   end
 
   def test_stdin
