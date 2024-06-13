27d5d1a1c731cc7ea8c5a9c9aa5f02259554c91f
kochi-echo <tnodtegrof@gmail.com>
2024-06-13 15:49:52 +0900

    メソッド削除

--- 05.wc/lib/lib_wc_command.rb
+++ 05.wc/lib/lib_wc_command.rb
@@ -1,43 +1,28 @@
-#!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def run_wc(inputs, options, input_type_argv)
-  files_data = collect_files_data(inputs, input_type_argv)
+def run_wc(argv, stdin, option)
+  contents_numbers = argv.empty? ? [content_numbers(stdin, '')] : collect_numbers(argv)
   display_keys = select_display_keys(options)
-  text = format_data(files_data, display_keys)
-  text.push(format_total(files_data, display_keys)) if files_data.size > 1
-  text.join("\n")
+  texts = format_texts(contents_numbers, display_keys)
+  texts.join("\n")
 end
 
-def collect_files_data(inputs, input_type_argv)
-  if input_type_argv
-    paths = inputs.flat_map { |input| Dir.glob(input) }
-    build_data(paths)
-  else
-    [file_data(inputs, '')]
-  end
-end
-
-def select_display_keys(options)
-  no_option = options.values.none?
-  {
-    row_number: no_option || options[:l],
-    word_number: no_option || options[:w],
-    bytesize: no_option || options[:c]
-  }.select { |_key, value| value }.keys
-end
-
-def build_data(paths)
-  paths.map do |path|
+def collect_numbers(argv)
+  argv.flat_map { |str| Dir.glob(str) }.map do |path|
     next { warning: "wc: #{path}: read: Is a directory" } if File.directory?(path)
 
     file = File.open(path)
     content = file.read
-    file_data(content, path)
+    content_numbers(content, path)
   end
 end
 
-def file_data(content, path)
+def select_display_keys(options)
+  numbers_type = { row_number: options[:l], word_number: options[:w], bytesize: options[:c]}
+  options.values.none? ? numbers_type.select { |_key, value| value }.keys : numbers_type.keys
+end
+
+def content_numbers(content, path)
   {
     row_number: content.lines.count,
     word_number: content.split.size,
@@ -46,19 +31,23 @@ def file_data(content, path)
   }
 end
 
-def format_data(files_data, display_keys)
-  files_data.map do |file_data|
-    next file_data[:warning] if file_data.key?(:warning)
+def format_texts(contents_numbers, display_keys)
+  total_numbers = display_keys.to_h { |key| [key, 0] }
+  lines_texts = contents_numbers.map do |content_numbers|
+    next content_numbers[:warning] if content_numbers.key?(:warning)
 
-    text = display_keys.map { |key| file_data[key].to_s.rjust(8) }
-    text.push(" #{file_data[:file_name]}") unless file_data[:file_name].empty?
-    text.join
+    texts_content_numbers(content_numbers, display_keys)
   end
+  lines_texts.push("#{total_numbers.values.map{ |value| value.to_s.rjust(8) }.join} total") if contents_numbers.size > 1
+  lines_texts.push("\n")
 end
 
-def format_total(files_data, display_keys)
-  text = display_keys.map do |key|
-    files_data.sum { |file_data| file_data.key?(key) ? file_data[key] : 0 }.to_s.rjust(8)
+def texts_content_numbers(content_numbers, display_keys)
+  texts = display_keys.map do |key|
+    num = content_numbers[key]
+    total_numbers[key] += num
+    num.to_s.rjust(8)
   end
-  text.push(' total').join
+  texts.push(" #{content_numbers[:file_name]}") unless file[:file_name].empty?
+  texts.join
 end
--- 05.wc/my-wc.rb
+++ 05.wc/my-wc.rb
@@ -2,7 +2,6 @@
 # frozen_string_literal: true
 
 require 'optparse'
-
 require_relative './lib/lib_wc_command'
 
 options = {}
@@ -11,10 +10,8 @@ opt.on('-l') { options[:l] = true }
 opt.on('-w') { options[:w] = true }
 opt.on('-c') { options[:c] = true }
 opt.parse!(ARGV) # オプション除いて残った引数
-inputs = [*ARGV]
 
-input_type_argv = inputs.empty? ? false : true
+stdin = $stdin.readlines.join
 
-inputs = $stdin.readlines.join unless input_type_argv
-
-puts run_wc(inputs || '.', options, input_type_argv)
+puts run_wc([*ARGV], stdin, options)
+# puts run_wc(inputs || '.', options, input_type_argv)
