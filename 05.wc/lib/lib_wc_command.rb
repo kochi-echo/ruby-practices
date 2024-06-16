cf235a18c6707192fd4ba74a2eedd0f8434c45c2
kochi-echo <tnodtegrof@gmail.com>
2024-06-16 10:21:03 +0900

    rubocop

--- 05.wc/lib/lib_wc_command.rb
+++ 05.wc/lib/lib_wc_command.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 def run_wc(argv, stdin, options)
-  contents = argv.nil? ? [{numbers: numbers(stdin)}] : collect_contents([*argv])
+  contents = argv.nil? ? [{ numbers: numbers(stdin) }] : collect_contents([*argv])
   # [*argv]はargv入力が一つのファイル指定の時str型で複数ファイル指定の時にarray型になるため
   format_texts(contents, options)
 end
@@ -12,7 +12,7 @@ def collect_contents(argvs)
   contents = paths.map do |path|
     next { warning: "wc: #{path}: read: Is a directory" } if File.directory?(path)
 
-    File.open(path) { |f| {numbers: numbers(f.read), file_name: path} }
+    File.open(path) { |f| { numbers: numbers(f.read), file_name: path } }
   end
   contents.size > 1 ? add_total(contents) : contents
 end
@@ -21,7 +21,7 @@ def numbers(str)
   {
     l: str.lines.count,
     w: str.split.size,
-    c: str.bytesize,
+    c: str.bytesize
   }
 end
 
@@ -31,7 +31,7 @@ def format_texts(contents, options)
   contents.map do |content|
     next content[:warning] if content.key?(:warning)
 
-    text = content[:numbers].map{ |key,value| value.to_s.rjust(8) if options[key] }
+    text = content[:numbers].map { |key, value| value.to_s.rjust(8) if options[key] }
     text.push(" #{content[:file_name]}") unless content[:file_name].nil?
     text.join
   end.join("\n")
@@ -45,5 +45,5 @@ def add_total(contents)
       total[key] += value
     end
   end
-  contents.push({numbers: total_numbers, file_name: 'total'})
+  contents.push({ numbers: total_numbers, file_name: 'total' })
 end
